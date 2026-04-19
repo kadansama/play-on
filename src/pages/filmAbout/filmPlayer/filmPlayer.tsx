86856fe66d73f111a4d2eb02426ddabb57110678
kadansama <aidarchess27@gmail.com>
2026-04-19 21:21:29 +0300

    обновлен плеер

--- src/pages/filmAbout/filmPlayer/filmPlayer.tsx
+++ src/pages/filmAbout/filmPlayer/filmPlayer.tsx
@@ -5,17 +5,23 @@ type FilmPlayerProps = {
     id: number
 }
 
-export const FilmPlayer = ({id}: FilmPlayerProps) => {
+export const FilmPlayer = ({ id }: FilmPlayerProps) => {
     return (
         <div>
-            <iframe 
+            {/* <iframe
                 className={styles.frame}
                 src={`https://fbfree.lol/series/${id}/`}
                 title={FILM_LABELS.PLAYER_TITLE}
                 allow="autoplay; fullscreen"
                 allowFullScreen
                 sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-            />
+            /> */}
+            <iframe src={`https://kinodivbox.github.io/iframe?id=${id}`}
+                className={styles.frame}
+                width="100%" height="600" allow="fullscreen" loading="lazy"
+                scrolling="no"
+                style={{ overflow: "hidden", border: "none" }}>
+            </iframe>
         </div>
     )
 }
